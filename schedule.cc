4edf924bfa60e92cd057d382c427b37203fdb6f7
Arthur Li <arthur.li@leicabiosystems.com>
2017-10-25 07:04:29 +0800

    update code

--- schedule.cc
+++ schedule.cc
@@ -1,15 +1,17 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <ctime>
 #include "json.hpp"
+#include <unistd.h>
 
 // for convenience
 using json = nlohmann::json;
 using namespace std;
 
-class configuration {
+class Configuration {
 public:
-    configuration()
+    Configuration()
     {
         restore();
         reagents = &_config["reagents"];
@@ -53,7 +55,7 @@ struct step {
     {
         id = j["id"].get<int>();
         reagent = j["reagent"].get<std::string>();
-        duration = j["duration"].get<int>();
+        duration = j["duration"].get<int>() * 60;
         temperature = j["temperature"].get<int>();
         is_pressure = (j["pressure"].get<std::string>() == "on") ? true : false;
         is_vacuum = (j["vacuum"].get<std::string>() == "on") ? true : false;
@@ -78,47 +80,63 @@ struct step {
 
 class Program {
 public:
-    Program(std::string _name, json &_j, int start_time_)
-        : start_time(0), end_time(0), offset(0), index(0), name(_name)
+    Program(std::string _name, json &_j, std::string start_time_, Configuration *config_, int retort_)
+        : end_time(0), offset(0), index(0), name(_name), config(config_), retort(retort_)
     {
         for (size_t i = 0; i < _j.size(); ++i)
             steps.insert(make_pair(i, new step(_j[i])));
+        struct tm tm_ = {0};
+        strptime(start_time_.c_str(), "%Y-%m-%d %H:%M:%S", &tm_);
+        start_time = mktime(&tm_);
     }
 
-    void dump() const
+    void print_timing_sequence()
     {
-        for (size_t i = 0; i < steps.size(); ++i)
-            std::cout << "\n\n----------------------\nstep : " << steps[i]->dump();
+        printf("\n\n------------------------------------------\n");
+        printf("        Program's name: %s\n", name.c_str());
+
+        time_t time_sum = start_time + offset;
+        char buf[30] = {0};
+
+        for (size_t i = 0; i < steps.size(); ++i) {
+            struct tm *t = localtime((time_t *)&time_sum);
+            strftime(buf, 50, "%Y-%m-%d %H:%M:%S", t);
+            time_sum += steps[i]->duration;
+            printf("%s: reagent: %s\n", buf,
+                config->reagents->operator[](steps[i]->reagent.substr(1)).operator[]("name").get<std::string>().c_str());
+        }
     }
 
-    int resolveConflicts(const Program& base)
+    std::string dump_end_time()
     {
-        int offset = 0;
-        base.steps[]
+        // for (size_t i = 0; i < program.steps.size(); ++i) {
 
+        // }    
     }
 
-    int sumOffset(const Program& program, int index)
+    int resolveConflicts(Program *base)
     {
-        int sum = 0;
-        for (int i = 0; i <= index; ++i)
-            sum += program.steps[i].duration;
-        return sum - runTime;
+        // int offset = 0;
+        // base.steps[]
+
     }
 
-    int find(const Program& program, std::string reagent)
+    int sumOffset(Program* program, int index)
+    {
+        // int sum = 0;
+        // for (int i = 0; i <= index; ++i)
+        //     sum += program.steps[i].duration;
+        // return sum - runTime;
+    }
+
+    int find(Program* program, std::string reagent)
     {
         int index = -1;
         if (reagent == "r1")
             return index;
-        for (size_t i = 0; i < program.steps.size(); ++i) {
-            if (program.steps[i].reagent == reagent) {
-                sum = 0;
-                for (size_t j = 0; j <= i; ++j) {
-                    sum += program.steps[i].duration;
-                }
-                if (sum > runTime)
-                    index = i;
+        for (size_t i = 0; i < program->steps.size(); ++i) {
+            if (program->steps[i]->reagent == reagent) {
+                index = i;
                 break;
             }
         }
@@ -126,7 +144,12 @@ public:
         return index;
     }
 
-private:
+    std::string getName()
+    {
+        return name;
+    }
+
+public:
     int runTime;
     int start_time;
     int offset;
@@ -134,34 +157,64 @@ private:
     int index;
     std::string name;
     std::map<int, step *> steps;
+    Configuration *config;
+    int retort;
 };
 
-class schedule_controller {
-public:
-    schedule_controller()
-    {
+// class schedule_controller {
+// public:
+//     schedule_controller()
+//     {
 
-    }
+//     }
 
-};
+// };
 
-class device_controller {
-public:
-    device_controller()
-    {
+// class device_controller {
+// public:
+//     device_controller()
+//     {
 
-    }
+//     }
 
-};
+// };
+
+int compare_time(const Program *p1, const Program *p2)
+{
+    return p1->start_time < p2->start_time;
+}
 
 int main()
 {
-	auto config = configuration();
+    auto config = Configuration();
+    
+    // load program from configuration
+    auto run = config.run;
+    vector<Program *> vector_;
+    for (size_t i = 0; i < run->size(); ++i) {
+        json &item = run->operator[](i);
+        // std::cout << "run: " << item.dump() << std::endl;
+        // std::cout << "program's name: " << item["program"].get<std::string>().substr(1) << std::endl;
+        std::string name = item["program"].get<std::string>().substr(1);
+        std::string start_time = item["start_time"].get<std::string>();
+        int retort = atoi(item["retort"].get<std::string>().substr(2).c_str());
+        json &j = config.programs->operator[](name);
+        auto p = new Program(name, j, start_time, &config, retort);
+        vector_.push_back(p);
+    }
 
-    json &overnight = config.programs->operator[]("overnight");
-    Program p("overnight", overnight);
-    p.dump();
+    // sort by start_time
+    sort(vector_.begin(), vector_.end(), compare_time);
 
+    // resolve conflicts
+    for (size_t i = 1; i < vector_.size(); ++i) {
+        Program *program = vector_[i];
+        program->resolveConflicts(vector_[i - 1]);
+    }
+
+    for (size_t i = 0; i < vector_.size(); ++i) {
+        vector_[i]->print_timing_sequence();
+    }
 #if 0
     json &cleaning = (*config.Programs)["overnight"];
     //json cleaning = Programs["cleaning"];
@@ -189,7 +242,7 @@ int main()
 
 
 
-
+    pause();
 
     return 0;
 }
